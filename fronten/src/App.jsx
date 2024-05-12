394ffb080cd9afef00d7e2ae2ff09d59f916769a
serbanples <serbanples@yahoo.com>
2024-05-12 21:55:21 +0300

    changed project to vite

---
+++ fronten/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import LogIn from './pages/auth/login/LogIn'
+import SignUp from './pages/auth/signup/SignUp'
+import Page from './pages/root/Page'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/login' element={ <LogIn /> } />
+      <Route path='/sign-up' element={ <SignUp /> } />
+      <Route path='/page' element={ <Page /> } />
+    </Routes>
+  )
+}
+
+export default App
